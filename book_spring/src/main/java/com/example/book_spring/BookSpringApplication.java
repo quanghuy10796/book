2f51ecf936a9399b55f52e5435ef885bdbd07cf2
quanghuy10796 <48671608+quanghuy10796@users.noreply.github.com>
2021-01-29 21:02:31 +0700

    init spring book

---
+++ book_spring/src/main/java/com/example/book_spring/BookSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSpringApplication.class, args);
+	}
+
+}
---
+++ book_spring/src/test/java/com/example/book_spring/BookSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
